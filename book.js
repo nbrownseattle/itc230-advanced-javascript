aaa86eca5d167c7fed7f687e1ba3a46f90c3d496
Nicole Brown <giantspork@gmail.com>
2018-04-26 01:15:37 +0000

    updating for express

---
+++ book.js
@@ -0,0 +1,45 @@
+/*Nicole Brown ITC 230 assignment 2 book.js file*/
+
+//array list of book titles
+//let books
+var books = [
+
+{title:"honey bee democracy",author:"thomas d seeley",pubdate:"2010"},
+{title:"beekeeping for dummies",author:"howland blackiston",pubdate:"2017"},
+{title:"the beekeeper's handbook",author:"diana sammataro",pubdate:"1973"},
+{title:"natural beekeeping: organic approaches to modern apiculture",author:"ross conrad",pubdate:"2013"},
+{title:"beekeeping for fun and profit",author:"cindy belknap",pubdate:"2011"},
+{title:"langstroth's hive and the honey bee",author:"l l lanstroth",pubdate:"1853"}
+
+];
+
+
+// Returns all array items
+exports.getAllBooks = function() {
+    return books;
+};
+
+//Returns one array item
+exports.findTitle = (title) => {
+    return books.find((item) => {
+        
+        return item.title == title;
+    });
+    
+};
+    
+//Deletes one item from the array
+exports.delete = (title) => {
+    const originalCount = books.length;
+    //filter creates new array
+    books = books.filter((item) =>{
+        return item.title.toLowerCase() != title;
+    });
+    
+//Boolean to idicate if item deleted
+return books.length != originalCount;
+    
+    };
+
+
+
--- main.js
+++ main.js
@@ -7,6 +7,8 @@
 
 var http = require("http"); 
 var fs = require("fs");
+var books = require('./book');
+
 
 http.createServer(function (req,res) {
     //changes url to lower
@@ -23,15 +25,37 @@ http.createServer(function (req,res) {
               console.log(data.toString());
             });
             break;
+            
         case '/about':
-              res.writeHead(200, {'Content-Type': 'text/plain'});
-              res.end('about page');
-              break;
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('about page');
+            break;
+       
+        case'/getall':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(JSON.stringify(books.getAllBooks()));
+            res.end('results');
+            break;
+            
+        case'/get':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(JSON.stringify(books.findTitle('beekeeping for dummies')));
+            res.end('results');
+            break;
+            
+        case'/delete':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(JSON.stringify(books.delete('beekeeping for dummies')));
+            res.end('results');
+            break; 
+            
         default:
             res.writeHead(404, {'Content-Type': 'text/plain'});
-              res.end('Not found');
-              break;
-            }
-        }).listen(process.env.PORT || process.env.IP); 
+            res.end('Not found');
+            break;
+            
+}
+        
+        }).listen(process.env.PORT || 3000); 
 
 
