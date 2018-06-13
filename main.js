a1886cb84518f7b0c69c1664511216b4ce7d9f96
Nicole Brown <giantspork@gmail.com>
2018-06-13 02:36:37 +0000

    initial commit for assignment 8

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 /* ITC 230 Nicole Brown. 
-* Assignment 6 Rest APIs
+* Assignment 8 React CRUD
 * PLEASE NOTE: File shoud be named index.js, I named it main.js by mistake. 
 */
 
 /*routing*/
 
 var Book = require('./models/bookdb');
-//var books = require('./book'); //remove this eventually
 'use strict';
 const express = require("express");
 const app = express();
@@ -21,10 +20,10 @@ app.set("view engine", ".html");
 
 // send static file as response
 // get all the books in the db
-app.get('/', (req, res) => {
- Book.find({}, (err,books) => {
- if (err) return (err);
- console.log(books.length);
+app.get('/', (req, res, next) => {
+ Book.find((err,books) => {
+     console.log(books);
+ if (err) return next (err);
  res.render('home_app', {books: JSON.stringify(books)});
  
  });
@@ -33,15 +32,17 @@ app.get('/', (req, res) => {
 
 /**************START OF API ROUTES*******************************/
 
-//Find one book in the db
-app.get('/api/book/:title', (req,res) => {
- Book.findOne({ title: req.params.title.toLowerCase()}, (err, book) => {
-  if(err) return (err);
-  res.json(book);
- });
+// find one book in the db collection
+app.get('/api/book/:title', (req, res, next) => {
+    let title = req.params.title;
+    console.log(title);
+    Book.findOne({title: title}, (err, result) => {
+        if (err || !result) return next(err);
+        res.json( result );    
+    });
 });
 
-//Gets all the books in the db
+// gets all the books in the db collection
 app.get('/api/book', (req, res) => {
  Book.find({}, (err,books) => {
   if (err) return (err);
@@ -51,110 +52,34 @@ app.get('/api/book', (req, res) => {
  });
 });
 
-//DANGER ZONE!!!!! Deletes one book
-app.get('/api/delete/:title', (req,res, next) => {
-   Book.remove({title: req.params.title }, (err, title) => {
-       //console.log(err);
+//DANGER ZONE!!!!! Deletes one book from the db collection
+app.get('/api/delete/:id', (req,res, next) => {
+   Book.remove({"_id":req.params.id}, (err, result) => {
         if (err) return next(err);
-           res.json(title);
+           res.json({"deleted": result.n});
        });
    }); 
  
- //Adds one book to the db
-app.get('/api/book/add/:title/:author/:isbn/:pubdate/:quantity', (req, res) => {
-   let obj = {
-       title: req.params.title,
-       author: req.params.author,
-       isbn: req.params.isbn,
-       pubdate: req.params.pubdate,
-       quantity: req.params.quantity
-   };
-   console.log(req.params.title);
-   Book.create(obj, (err, book) => {
-       if(err) return (err);
-       res.json(book);
-   });
-
-});
-
-
-//END API ROUTES  
-
-/*******START OF old db SCRIPTS**************************************/
-
-//Find one book in the db
-app.get('/get', (req,res) => {
- console.log(req.query.title);
- Book.findOne({ title: req.query.title.toLowerCase()}, (err, book) => {
-  if(err) return (err);
-  res.type('text/html');
- res.render('details', {title: req.query.title, result: book });
- });
-});
-
-// Post one book from the db
-app.post('/get', (req,res) => {
- Book.findOne({title: req.body.title}, (err, book) => {
- if (err) return (err);
-  res.type('text/html');
-  res.render('details', { title: req.body.title, book });
-  });
-});
-
-// get all the books in the db
-app.get('/getAll', (req, res) => {
- Book.find({}, (err,books) => {
- if (err) return (err);
- console.log(books.length);
- res.render('home', {books: books });
- 
- });
-});
-
-//DANGER ZONE!!!!! Deletes one book
-app.get('/delete', (req,res, next) => {
-   Book.remove({title: req.query.title }, (err, result) => {
-       //console.log(err);
-      let deleted = result;
-      //console.log(deleted);
-        if (err) return next(err);
-            res.type('text/html'); 
-            res.render('delete', {title: req.query.title, deleted: deleted});
-       });
-   }); 
-   
-
-//Updates a book. This works, but needs a UI
-app.post('/add', (req, res, next) => {
-   Book.update({"title": req.body.title, 
-   "author": req.body.author, 
-   "isbn": req.body.isbn, 
-   "pubdate": req.body.pubdate,  
-   "quantity": req.body.quantity } , (err, result) => {
-      if (err) return next(err);
-       let added = result;
-            res.type('text/html');
-            res.render('add', {title: req.body.title, added: added});
+ // adds or updates one book to the db collection
+app.post('/api/add/', (req, res, next) => {
+    // find and update or add new book
+  if (!req.body._id) { // insert new book
+        let book = new Book({title:req.body.title,author:req.body.author,isbn:req.body.isbn,pubdate:req.body.pubdate,quantity:req.body.quantity});
+        book.save((err,newBook) => {
+            if (err) return next(err);
+            console.log(newBook);
+            res.json({updated: 0, _id: newBook._id});
         });
-    }); 
-
-//Adds new book. This approach works
-app.post('/add', (req, res, next) => {
-    let obj = {
-        title: req.body.title, 
-        author: req.body.author,
-        isbn: req.body.isbn,
-        pubdate: req.body.pubdate,
-        quantity: req.body.quantity,
-    };
-    
-    Book.create(obj, (err, book) =>{
-        if(err) return next(err);
-        res.type('text/html');
-        res.render('add', {result: Book, title: req.query.title});
-    });
+    } else { // update existing book
+        Book.updateOne({ _id: req.body._id}, {title:req.body.title,author:req.body.author,isbn:req.body.isbn,pubdate:req.body.pubdate,quantity:req.body.quantity}, (err, result) => {
+            if (err) return next(err);
+            res.json({updated: result.nModified, _id: req.body._id});
+        });
+    }
 });
 
+
+/****************END API ROUTES***********************************/  
 
 // define 404 handler
 app.use((req,res) => {
@@ -166,6 +91,5 @@ app.use((req,res) => {
 app.listen(app.get('port'), () => {
  console.log('Express started'); 
 });
-
 
 
